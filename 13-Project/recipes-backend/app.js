6de10a6904ce157408ad69e01a139b592b47318c
Sagie Bar Hama <58434974+Sagie-BH@users.noreply.github.com>
2023-08-20 23:49:08 +0300

    backend comments

--- 13-Project/recipes-backend/app.js
+++ 13-Project/recipes-backend/app.js
@@ -1,3 +1,13 @@
+/*
+    Purpose: Initializes the server, sets up middleware, and listens on a specified port.
+    Functionality:
+
+    Uses express for server creation and routing.
+    Uses body-parser to parse incoming request bodies.
+    Uses cors to enable Cross-Origin Resource Sharing.
+    Imports and uses recipeRoutes for handling recipe-related routes.
+*/
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -7,16 +17,21 @@ const app = express();
 
 const port = 3300;
 
+// Middleware to parse incoming request bodies.
 app.use(bodyParser.json());
 
+// Middleware to enable Cross-Origin Resource Sharing.
 app.use(cors());
 
+// Use the recipe routes for handling recipe-related endpoints.
 app.use(recipeRoutes);
 
+// Middleware to handle errors.
 app.use((err, req, res, next) => {
     console.log(err);
 });
 
+// Start the server and listen on the specified port.
 app.listen(port, () => {
     console.log('Server is running');
 });
--- 13-Project/recipes-backend/routes/recipeRoutes.js
+++ 13-Project/recipes-backend/routes/recipeRoutes.js
@@ -1,78 +1,120 @@
-const express = require('express');
-const axios = require('axios');
-const fs = require('fs');
-const path = require('path');
+/*
+    Purpose: Defines routes related to recipes, including fetching recipes based on ingredients and managing reviews.
+    Functionality:
 
-const recipesFilePath = path.join(__dirname, 'recipes.json');
-
-const appId = '29c38e73';
-const appKey = 'd39ac38d6045736379b1e3f28762cbff';
-
-const router = express.Router();
-
-const findRecipeByLabel = (label, recipes) => {
-    return recipes.find(recipe => recipe.label === label);
-}
-
-router.get('/recipes/:ingredient', async (req, res) => {
-    const ingredient = req.params.ingredient;
-    try {
-        const response = await axios.get(`https://api.edamam.com/search?q=${ingredient}&app_id=${appId}&app_key=${appKey}`);
-        const recipes = response.data.hits.map(hit => ({
-            label: hit.recipe.label,
-            image: hit.recipe.image,
-            uri: hit.recipe.uri,
-            ingredients: hit.recipe.ingredients
-        }));
-        // Gets recipes if there is a json file
-        const allRecipes = fs.existsSync(recipesFilePath) ?
-         JSON.parse(fs.readFileSync(recipesFilePath, 'utf8')) : [];
-
-        const recipesToSend = recipes.map(incomingRecipe => {
-            const existingRecipe = findRecipeByLabel(incomingRecipe.label, allRecipes);
-            if(existingRecipe) {
-                incomingRecipe.reviews = existingRecipe.reviews
-            } else {
-                incomingRecipe.reviews = [];
-                allRecipes.push(incomingRecipe);
-            }
-            return incomingRecipe;
-        });
-
-        fs.writeFileSync(recipesFilePath, JSON.stringify(allRecipes, null, 2), 'utf8');
-
-        res.status(200).json(recipesToSend);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
+    Uses express for routing.
+    Uses axios to make HTTP requests to the Edamam API.
+    Uses fs for file system operations (reading and writing recipes to a JSON file).
+    Uses path to construct the path to the recipes JSON file.
+    Defines a GET route to fetch recipes based on an ingredient.
+    Defines a POST route to add reviews to a specific recipe.
+ */
+    const express = require('express');
+    const axios = require('axios');
+    const fs = require('fs');
+    const path = require('path');
+    
+    // Construct the path to the recipes JSON file.
+    // `__dirname` is a Node.js global variable that returns the directory path of the currently executing script.
+    const recipesFilePath = path.join(__dirname, 'recipes.json');
+    
+    // Edamam API credentials.
+    const appId = '29c38e73';
+    const appKey = 'd39ac38d6045736379b1e3f28762cbff';
+    
+    // `router` is an instance of Express's `Router` class. 
+    // It allows modularization of route handlers,
+    // enabling cleaner and more organized code.
+    // With `router`, you can define multiple routes and then
+    // mount them on the main application using a single middleware.
+    const router = express.Router();
+    
+    // Helper function to find a recipe by its label.
+    const findRecipeByLabel = (label, recipes) => {
+        return recipes.find(recipe => recipe.label === label);
     }
-});
+    
+    // GET route to fetch recipes based on an ingredient.
+    router.get('/recipes/:ingredient', async (req, res) => {
+        const ingredient = req.params.ingredient;
+        try {
+            // Fetch recipes from the Edamam API based on the ingredient.
+            const response = await axios.get(`https://api.edamam.com/search?q=${ingredient}&app_id=${appId}&app_key=${appKey}`);
+            
+            /* Extract relevant details from the API response.
+            Filtering the data to only what the frontend needs:
 
-router.post('/recipes/reviews/:label', (req, res) => {
-    const name = req.params.label;
-    const { review } = req.body;
-
-    // Check if recipes file exists, create if not
-    if (!fs.existsSync(recipesFilePath)) {
-        fs.writeFileSync(recipesFilePath, '[]', 'utf8');
-    }
-
-    // Read recipes from the file
-    const recipes = JSON.parse(fs.readFileSync(recipesFilePath, 'utf8'));
-
-    const recipe = recipes.find(recipe => recipe.label === name);
-
-    if (recipe) {
-        recipe.reviews = recipe.reviews || [];
-        recipe.reviews.push(review);
-
-        fs.writeFileSync(recipesFilePath, JSON.stringify(recipes, null, 2), 'utf8');
-
-        res.json(recipe.reviews);
-    } else {
-        res.status(404).json({ error: 'Recipe not found' });
-    }
-
-})
-
-module.exports = router;
+                1. Performance: Reducing the amount of data transferred
+                    can lead to faster load times and a more responsive user experience.
+                2. Security: Minimizing the exposure of data 
+                    can prevent potential data leaks or unintended sharing of sensitive information.
+                3. Simplicity: Sending only necessary data makes frontend
+                    processing simpler and reduces the potential for errors or bugs.
+             */
+            const recipes = response.data.hits.map(hit => ({
+                label: hit.recipe.label,
+                image: hit.recipe.image,
+                uri: hit.recipe.uri,
+                ingredients: hit.recipe.ingredients
+            }));
+    
+            // Check if there's a local recipes JSON file and read from it. if not initializes an empty array
+            const allRecipes = fs.existsSync(recipesFilePath) ?
+             JSON.parse(fs.readFileSync(recipesFilePath, 'utf8')) : [];
+    
+            // Merge incoming recipes with existing ones, adding reviews if they exist.
+            const recipesToSend = recipes.map(incomingRecipe => {
+                const existingRecipe = findRecipeByLabel(incomingRecipe.label, allRecipes);
+                if(existingRecipe) {
+                    incomingRecipe.reviews = existingRecipe.reviews
+                } else {
+                    incomingRecipe.reviews = [];
+                    allRecipes.push(incomingRecipe);
+                }
+                return incomingRecipe;
+            });
+    
+            // Save the merged list back to the JSON file.
+            fs.writeFileSync(recipesFilePath, JSON.stringify(allRecipes, null, 2), 'utf8');
+    
+            // Send the merged list as the response.
+            res.status(200).json(recipesToSend);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    });
+    
+    // POST route to add reviews to a specific recipe.
+    router.post('/recipes/reviews/:label', (req, res) => {
+        const name = req.params.label;
+        const { review } = req.body;
+    
+        // Check if the recipes JSON file exists, if not, create it.
+        if (!fs.existsSync(recipesFilePath)) {
+            fs.writeFileSync(recipesFilePath, '[]', 'utf8');
+        }
+    
+        // Read existing recipes from the JSON file.
+        const recipes = JSON.parse(fs.readFileSync(recipesFilePath, 'utf8'));
+    
+        // Find the recipe by its label.
+        const recipe = recipes.find(recipe => recipe.label === name);
+    
+        // If the recipe exists, add the review.
+        if (recipe) {
+            recipe.reviews = recipe.reviews || [];
+            recipe.reviews.push(review);
+    
+            // Save the updated recipes list back to the JSON file.
+            fs.writeFileSync(recipesFilePath, JSON.stringify(recipes, null, 2), 'utf8');
+    
+            // Send the updated reviews as the response.
+            res.json(recipe.reviews);
+        } else {
+            res.status(404).json({ error: 'Recipe not found' });
+        }
+    })
+    
+    module.exports = router;
+    
