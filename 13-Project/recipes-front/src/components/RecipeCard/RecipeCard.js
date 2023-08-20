9e012ff05e5869703b0e152b071d72f438454ad0
Sagie Bar Hama <58434974+Sagie-BH@users.noreply.github.com>
2023-08-20 23:35:42 +0300

    comments

--- 13-Project/recipes-front/src/components/RecipeCard/RecipeCard.js
+++ 13-Project/recipes-front/src/components/RecipeCard/RecipeCard.js
@@ -1,31 +1,71 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+/*
+useState is a Hook in React that lets you add state management to functional components. 
+State in React represents any data that might change over time and affects what is rendered on the screen. 
+By using useState, you can declare a state variable and a function to update it. 
+
+For example:
+const [count, setCount] = useState(0);
+
+Here, `count` is the state variable initialized with a value of 0, and `setCount` is the function to update it.
+
+When you update the state using the provided function (like setCount), React schedules a re-render of the component. 
+This means that the UI will automatically reflect the changes in the state. This reactivity is one of the core features 
+of React, ensuring the UI is always in sync with the underlying data.
+
+Without using state (like useState), changes to variables won't trigger a re-render, and thus the UI won't update to reflect those changes.
+*/
 import styles from './RecipeCard.module.css';
+/*
+CSS Modules is a technique in which you write CSS in a way that's scoped to a single component. 
+When you import styles from a `.module.css` file, you get an object with class names that are locally scoped to the component.
+
+Advantages:
+1. **Local Scope**: Each CSS class is scoped to the component, preventing global side effects. This means you can have 
+   the same class name in different module files without worrying about clashes.
+2. **Eliminates Naming Collisions**: Since class names are locally scoped, you don't have to worry about naming collisions 
+   in the global namespace.
+3. **Better Code Organization**: It's clear which styles are used by which component, making the codebase easier to maintain.
+4. **Performance**: Only the styles that are used by a component are loaded, rather than loading a large global CSS file. 
+   This can lead to faster load times.
+
+By using CSS Modules, you avoid the pitfalls of global CSS, where styles from one part of an application might inadvertently 
+affect another part. Instead, each component gets its own set of styles, ensuring that components are truly encapsulated.
+*/
 import axios from 'axios';
 
+/* 
+  RecipeCard Component:
+  Functionality: Showcases individual recipe details, allows users to submit and view reviews.
+  Use: Represents a single recipe, displaying its information and associated reviews.
+*/
 const RecipeCard = ({ recipe }) => {
-  const [review, setReview] = useState("");
-  const [reviews, setReviews] = useState(recipe.reviews || []);
+  const [review, setReview] = useState(""); // State for the new review input.
+  const [reviews, setReviews] = useState(recipe.reviews || []); // State for the list of reviews.
 
-
+  // Mapping over the ingredients to get a list of ingredient text for the display.
   const ingredients = recipe.ingredients.map(i => i.text);
 
+  // Function to handle the submission of a review.
   const handleReviewSubmit = async () => {
     try {
       if (review.trim() === ""){
         console.log('No review added');
         return;
       } 
-      const response = await axios.post(`
-      http://localhost:3300/recipes/reviews/${recipe.label}`, { review });
+      // Sending a POST request to add a review for the given recipe and getting back the updated review list.
+      const response = await axios.post(`http://localhost:3300/recipes/reviews/${recipe.label}`, { review });
 
+      // Updating the reviews state with the new list of reviews.
       setReviews(response.data);
-      setReview('');
+      setReview(''); // Clearing the review input, to make room for new input
 
     } catch(err) {
       console.error("Error submitting review:", err);
     }
   }
 
+  // Rendering the RecipeCard component.
   return (
     <div className={styles.recipeCard}>
       <div className={styles.details}>
--- 13-Project/recipes-front/src/components/RecipeList/RecipeList.js
+++ 13-Project/recipes-front/src/components/RecipeList/RecipeList.js
@@ -1,16 +1,24 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import RecipeCard from "../RecipeCard/RecipeCard";
-import styles from './RecipeList.module.css'
+import axios from "axios"; // Importing axios for HTTP requests.
+import { useState } from "react"; // Importing hooks: useState for state management.
+import RecipeCard from "../RecipeCard/RecipeCard"; // Importing the RecipeCard component.
+import styles from './RecipeList.module.css' // Importing CSS module for styling.
 
+/* 
+    RecipeList Component:
+    Functionality: Allows users to search for recipes based on an ingredient and displays the resulting recipes.
+    Use: Acts as a container for the search feature and lists individual RecipeCard components.
+*/
 const RecipeList = () => {
-    const [ingredient, setIngredient] = useState('');
-    const [recipes, setRecipes] = useState([]);
+    const [ingredient, setIngredient] = useState(''); // State for the ingredient search input.
+    const [recipes, setRecipes] = useState([]); // State for the list of recipes.
 
+    // Function to handle the search for recipes based on the ingredient.
     const handleSearch = async () => {
         try {
+            // Sending a GET request to fetch recipes based on the ingredient.
             const response = await axios.get(`http://localhost:3300/recipes/${ingredient}`);
 
+            // Updating the recipes state with the fetched recipes.
             setRecipes(response.data);
         } catch (err) {
             console.error("Error getting recipes");
